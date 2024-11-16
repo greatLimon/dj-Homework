717d200fa6ad6bfd397e613477729212b1f23647
Ivan <gordeevid@yandex.ru>
2024-11-16 13:05:49 +0300

    Homework 4.2 fix 1

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,15 +1,30 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from .models import Article, Tags, TagsArticles
 
+class TagsArticlesInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main = False
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') == True and is_main == False:
+                is_main = True
+            elif form.cleaned_data.get('is_main') == True and is_main == True:
+                raise ValidationError('Основной тэг должен быть всегда один!')
+        if not is_main:
+            raise ValidationError('Укажите хотябы один основной тэг!')
+        return super().clean()
 
 class TagsArticlesInline(admin.TabularInline):
     model = TagsArticles
+    formset = TagsArticlesInlineFormset
     extra = 1
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    inlines = [TagsArticlesInline, ]
+    inlines = [TagsArticlesInline]
 
 @admin.register(Tags)
 class TagsAdm(admin.ModelAdmin):
