0ff915144d347aaa742bc8932e1c0cb0a1fb1f1b
Ivan <gordeevid@yandex.ru>
2024-12-22 13:07:09 +0300

    Homework 3.2 is fixed

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -8,6 +10,9 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', ]
+    search_fields = ['description', ]
     # при необходимости добавьте параметры фильтрации
 
 
@@ -15,6 +20,6 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
-    # filter_backends = ['DjangoFilterBackend']
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['products',]
   
